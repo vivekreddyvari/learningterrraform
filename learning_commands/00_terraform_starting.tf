38cdc063ff5b6528812358a9f8d32c3de3338ed4
vivekreddyvari <47451566+vivekreddyvari@users.noreply.github.com>
2024-06-23 23:06:32 +0200

    learning_commit

---
+++ learning_commands/00_terraform_starting.tf
@@ -0,0 +1,26 @@
+Learning Terraform
+
+1. Installation: MAC-OS
+-----------------------
+  brew tap hashicorp/tap
+  brew install terraform 
+
+1.1 Upgrading Terraform
+-----------------------
+  brew upgrade hashicorp/tap/terraform
+
+1.2 Verify the installation
+---------------------------
+  terraform -help
+
+1.2.1 Add any sub-command to terraform -help to learn more about what it does:
+-----------------------------------------------------------------------------
+  terraform -help plan
+
+
+-- end - 23jun2023
+
+
+
+
+
